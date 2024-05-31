5daa7117da7d4713b45afab13416e32e2ae43767
maximet <maxime.treca@adyen.com>
2024-05-31 10:07:17 +0200

    Add MacOS colors

--- scripts/generate.js
+++ scripts/generate.js
@@ -13,7 +13,7 @@ const tinycolor = require('tinycolor2');
 
 /**
  * @typedef {Object} Theme - Parsed theme object.
- * @prop {Record<'base'|'ansi'|'brightOther'|'other', string[]>} dracula - Dracula color variables.
+ * @prop {Record<'base'|'ansi'|'other', string[]>} dracula - Dracula color variables.
  * @prop {Record<string, string|null|undefined>} colors - VSCode color mapping.
  * @prop {TokenColor[]} tokenColors - Textmate token colors.
  */
@@ -37,7 +37,7 @@ const schema = DEFAULT_SCHEMA.extend([withAlphaType]);
 const transformSoft = theme => {
     /** @type {Theme} */
     const soft = JSON.parse(JSON.stringify(theme));
-    const brightColors = [...soft.dracula.ansi, ...soft.dracula.brightOther];
+    const brightColors = [...soft.dracula.ansi];
     for (const key of Object.keys(soft.colors)) {
         if (brightColors.includes(soft.colors[key])) {
             soft.colors[key] = tinycolor(soft.colors[key])
